3a22114ac6dd714b2739227120f15adbc27ae73b
lenawang101 <lenawxj@gmail.com>
2022-07-01 11:03:02 -0400

    completed website from introductory course

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode, useState } from "react";
 import SearchParams from "./SearchParams";
-import { StrictMode } from "react";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("green");
   return(
     <StrictMode>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
